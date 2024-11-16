dd37647f438452e7c8aece67002f2e1ff5a70577
KitKat-commits <ohmyglob319@gmail.com>
2024-11-15 20:18:53 -0500

    init commit

---
+++ src/main/java/net/cordeliad/timbermod/TimberMod.java
@@ -0,0 +1,15 @@
+package net.cordeliad.timbermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimberMod implements ModInitializer {
+	public static final String MOD_ID = "timbermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/cordeliad/timbermod/TimberModClient.java
@@ -0,0 +1,10 @@
+package net.cordeliad.timbermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimberModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
